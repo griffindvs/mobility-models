eee7684a01c04c38fcdfab1c3e05cea7afbe8093
Griffin Davis <contact@griffindvs.com>
2023-03-14 20:51:01 -0500

    update conversion program

--- utils/convert_state_dict.py
+++ utils/convert_state_dict.py
@@ -1,7 +1,16 @@
 import torch
 
+class EMNet(torch.nn.Module):
+    def __init__(self, features=22, hidden_dim=10, out_dim=2):
+        super(EMNet, self).__init__()
+        self.seq = torch.nn.Sequential(
+            torch.nn.Linear(features, hidden_dim),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(hidden_dim, out_dim),
+            torch.nn.ReLU()
+       	)
+
 def convert_state_dict(model_pt_path):
-    sample = torch.rand(22)
     model = torch.load(f"../{model_pt_path}")
     torch.save(model.state_dict(), f"../trained/{model_pt_path}")
 
